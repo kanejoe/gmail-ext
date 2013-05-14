4fba55541e8b4541d179374d1663529e3bdd6fe0
kanejoe <kane_joseph@hotmail.com>
2013-05-14 23:37:42 +0100

    logging

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ function onRequest(request, sender, sendResponse) {
   // Show the page action for the tab that the sender (content script)
   // was on.
   chrome.pageAction.show(sender.tab.id);
-
+console.dir(sender);
   // Return nothing to let the connection be cleaned up.
   sendResponse({});
 };
