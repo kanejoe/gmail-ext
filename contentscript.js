d0746e8b4b9c85e12fec41bdf9f11dd87dbfd3cb
kanejoe <kane_joseph@hotmail.com>
2013-05-15 17:35:12 +0100

    ex of adding dom elements

--- contentscript.js
+++ contentscript.js
@@ -7,8 +7,19 @@ var regex = /sandwich/;
 
 // Test the text of the body element against our regular expression.
 if (regex.test(document.body.innerText)) {
+	console.dir("it registers in 1-contentscript.js");
 	// The regular expression produced a match, so notify the background page.
 	chrome.extension.sendRequest({}, function(response) {});
 } else {
   // No match was found.
+}
+
+console.dir("it registers in 2-contentscript.js");
+
+if(document.getElementById('firstHeading')) {
+	var d = document.getElementById('firstHeading');
+	var newDiv = document.createElement("div");
+	var newContent = document.createTextNode("Hi there and greetings!");
+	newDiv.appendChild(newContent); //add the text node to the newly created div.
+	d.appendChild(newDiv);
 }
