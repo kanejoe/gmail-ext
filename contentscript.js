44a516ce662e3fd2b2ad1a348544e4fc54bf5c83
kanejoe <kane_joseph@hotmail.com>
2013-05-15 20:51:13 +0100

    adding cloned element in gmail

--- contentscript.js
+++ contentscript.js
@@ -16,10 +16,54 @@ if (regex.test(document.body.innerText)) {
 
 console.dir("it registers in 2-contentscript.js");
 
-if(document.getElementById('firstHeading')) {
-	var d = document.getElementById('firstHeading');
-	var newDiv = document.createElement("div");
-	var newContent = document.createTextNode("Hi there and greetings!");
-	newDiv.appendChild(newContent); //add the text node to the newly created div.
-	d.appendChild(newDiv);
-}
+// Javascript Enclosure 
+(function () {
+    var head;
+    var max_retry = 200;
+    
+    // Check if Gmail UI frame is ready 
+    function isGmailUIFrame(doc) {
+        try {
+            return document.getElementsByClassName('aic').length > 0;
+        } catch (e) {
+            return false;
+        }
+    }
+	
+	var createScriptElement = function(h) {
+		var d,i,c,par;
+		if (h.getElementsByClassName("G-Ni J-J5-Ji")){
+			d=h.getElementsByClassName("G-Ni J-J5-Ji")[3];
+			c=d.cloneNode(true);
+			
+			par = d.parentNode;
+			par.insertBefore(c, d);
+		}
+	};
+	
+    // Loop to check if the Gmail UI is loaded
+    var waitForGmailToLoad = function() {
+        var top_frame, canvas_frame;
+        try {
+            top_frame = window.top.document;
+            if (top_frame.getElementById('canvas_frame')) {
+            }
+        } catch (e) {}
+        top_frame = window.document;
+ 
+        if(top_frame && isGmailUIFrame(top_frame)) 
+        {
+            head = top_frame;
+            // Gmail UI is loaded: insert the script elements
+            createScriptElement(head);
+            return head;
+        }
+        else{
+            max_retry = max_retry -1;
+            if(max_retry > 0)
+                window.setTimeout(waitForGmailToLoad, 500);
+        }
+        return (head !== undefined);
+    };
+    waitForGmailToLoad();
+}());
